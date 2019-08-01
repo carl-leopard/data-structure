208e997f3eef8e692521bda31e809308d0fba12b
lewis <1374413525@qq.com>
2019-08-01 23:57:38 +0800

    breadthFirstSearch.go

---
+++ breadthFirstSearch.go
@@ -0,0 +1,123 @@
+package main
+
+import "fmt"
+
+type BinaryNode struct {
+	val   int
+	left  *BinaryNode
+	right *BinaryNode
+}
+
+type List struct {
+	node *BinaryNode
+	next *List
+}
+
+func main() {
+	root := &BinaryNode{
+		val: 0,
+	}
+
+	root = initTree(root)
+
+	list := bfs(root)
+	showList(list)
+}
+
+func initTree(root *BinaryNode) *BinaryNode{
+	head := root
+
+		
+	root.left =  &BinaryNode{
+		val: 1,
+		left: &BinaryNode{
+			val: 3,
+			left: &BinaryNode{
+				val: 7,
+			},
+			right: &BinaryNode{
+				val: 11,
+			},
+		},
+		right: &BinaryNode{
+			val: 4,
+			left: &BinaryNode{
+				val: 8,
+			},
+		},
+	}
+	root.right =  &BinaryNode{
+		val: 2,
+		left: &BinaryNode{
+			val: 5,
+			left: &BinaryNode{
+				val: 9,
+			},
+			right: &BinaryNode{
+				val: 12,
+			},
+		},
+		right: &BinaryNode{
+			val: 6,
+			right: &BinaryNode{
+				val: 10,
+			},
+		},
+	}
+
+	return head
+}
+
+func bfs(root *BinaryNode) *List {
+	result := new(List)
+	if root == nil || root.left == nil && root.right == nil {
+		result.next = &List{
+			node: root,
+		}
+		return result
+	}
+
+	head := result
+	var stack []*BinaryNode
+	var cur *BinaryNode
+	visitM := make(map[*BinaryNode]bool)
+
+
+	stack = append(stack, root)
+	for len(stack) > 0 {
+		cur = stack[0]
+		if !visitM[cur] {
+			if cur.left != nil {
+				stack = append(stack, cur.left)
+			}
+
+			if cur.right != nil {
+				stack = append(stack, cur.right)
+			}
+
+			
+			l := &List{
+				node: cur,
+			}
+			result.next = l
+			result = result.next
+
+			visitM[cur]= true
+		}
+		
+		stack = stack[1:]
+	}
+
+	return head
+}
+
+func showList(l *List) {
+	l = l.next
+
+	fmt.Printf("head")
+	for l != nil {
+		fmt.Printf("->%d", l.node.val)
+		l = l.next
+	}
+	fmt.Println()
+}
